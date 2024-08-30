873e6a16211bdd613d3c7874429b0449290c5579
KritikWankhede <nayanaamkyarakhutoh@gmail.com>
2024-08-31 00:48:50 +0530

    Without DC

--- momentJs/momentJs.js
+++ momentJs/momentJs.js
@@ -1,17 +1,29 @@
-import { LightningElement } from 'lwc';
-import MOMENT from '@salesforce/resourceUrl/moment';
-import {loadScript} from 'lightning/platformResourceLoader';
+    import { LightningElement } from 'lwc';
+    import MOMENT from '@salesforce/resourceUrl/moment';
+    import {loadScript,loadStyle} from 'lightning/platformResourceLoader';
+    import ANIMATE from '@salesforce/resourceUrl/animate';
 
-export default class MomentJs extends LightningElement {
+    export default class MomentJs extends LightningElement {
     currentDate;
+    isLoaded=false;
+
+
     renderedCallback(){
+    if(this.isLoaded){
+        return;
+    }
+    else{
         Promise.all([
-            loadScript(this, MOMENT + '/moment/moment.min.js')
+            loadScript(this, MOMENT + '/moment/moment.min.js'),
+            loadStyle(this,ANIMATE + '/animate/animate.min.css')
         ]).then(()=>{
                 this.setDateOnScreen();
         }).catch(error=>{
             console.error(error);
         })
+        this.isLoaded=true;
+        }
+
     }
 
     setDateOnScreen(){
